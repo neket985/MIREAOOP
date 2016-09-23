571a326c6e1e82e22025c730397640934a079502
Максик <hui>
2016-09-23 15:57:48 +0300

    добавил название лаб и их описание

---
+++ src/labs/labs1/Main.java
@@ -0,0 +1,8 @@
+package labs.labs1;
+
+/**
+ * Created by liveuser on 23.09.16.
+ */
+public class Main {
+}
+//Делать не надо
---
+++ src/labs/labs2/Main.java
@@ -0,0 +1,9 @@
+package labs.labs2;
+
+
+public class Main {
+    public static void main(String[] args) {
+//из второй лабораирки в конце надо выбрать вариант(у меня 6)+выбрать любой метод сортировки
+
+    }
+}
