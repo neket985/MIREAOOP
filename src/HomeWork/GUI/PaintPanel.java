6a03d41f430681f383d591b44bba4f15944a41fe
Makcuk <hui>
2016-10-13 22:25:36 +0300

    разноцветные многоугольники

--- src/HomeWork/GUI/PaintPanel.java
+++ src/HomeWork/GUI/PaintPanel.java
@@ -10,6 +10,20 @@ class PaintPanel extends JPanel
     @Override
     public void paint(Graphics g)
     {
+        //рандомные прямоугольники
+        g.setColor(Color.blue);
+        for (int i = 0; i < 3; i++){
+            //g.drawRect(rand.nextInt(400), rand.nextInt(400), rand.nextInt(400), rand.nextInt(400));
+            g.fillRect(rand.nextInt(400), rand.nextInt(400), rand.nextInt(400), rand.nextInt(400));
+        }
+        g.setColor(Color.cyan);
+        //рандомные квадраты
+        for (int i = 0; i < 3; i++){
+            int rando;
+            rando = rand.nextInt(400);
+            g.drawRect(rand.nextInt(400), rand.nextInt(400), rando, rando);
+        }
+        g.setColor(Color.green);
         g.drawOval(5, 0, 690, 735);
         for (int i = 0; i < 3; i++){
          drawCircle(g, rand.nextInt(400), rand.nextInt(400), rand.nextInt(400));
