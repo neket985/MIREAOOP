9a9bec81a7244377b1cf30ccbcd6ee245f3bb150
nikitos <neket98@bk.ru>
2016-10-28 16:05:47 +0300

    commit

---
+++ src/HomeWork/GetersSeters/Dog.java
@@ -0,0 +1,31 @@
+package homework.GetersSeters;
+
+
+public class Dog {
+
+    private double age;
+
+    public Dog(double age) {
+        this.age = age;
+    }
+
+    public double getAge() {
+        return age;
+    }
+//gnjksfhgkldfjgkl
+    public void setAge(double age) {
+        this.age = age;
+    }
+    public double realityAgeHuman(){
+        return age*7;
+    }
+
+    @Override
+    public String toString() {
+        return "переопределили метод ту стринг";
+    }
+
+    public  String getSystemInfo(){
+        return "то же самое что и метод тустринг, только получше";
+    }
+}
---
+++ src/HomeWork/GetersSeters/GetterSetter.java
@@ -0,0 +1,16 @@
+package homework.GetersSeters;
+public class GetterSetter {
+    private int val;
+
+    public GetterSetter(){}
+
+
+    public int getVal(){
+        return val;
+    }
+
+    public void setVal(int v){
+        val = v;
+    }
+
+}
